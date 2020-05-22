62c1145f335db9c7c40c9003991519088c15c31f
smileycat <liaocrong@outlook.com>
2020-05-22 11:49:36 +1000

    Updated summary

--- src/About.js
+++ src/About.js
@@ -6,31 +6,33 @@ function About() {
     <>
       <section id="about" className="container-md">
         <div className="ml-3 flex-100">
-          <h1 className="title mb-6">Chih-Jung Liao</h1>
+          <h1 className="title mb-6">David Liao</h1>
         </div>
         <div className="mr-3 ml-3 flex-1">
-          <div className="description">
+          <article className="description">
             <i className="mr-2 fas fa-map-marker-alt"></i>
             <span>Sydney, NSW 2007</span>
             <p className="text-justify">
-              I'm a recent graduate from Computer Science who loves doing
-              software development and really enjoyed doing web projects. I have
-              solid fundamentals in data structure, object-oriented programming,
-              HTTP protocols, and personally having Java, JavaScript being my
-              strong programming language to work with. Now I'm working myself
-              to become a great full stack developer.
+              I am a graduate with Bachelor Computer Science who loves doing
+              software development and making web projects. I have solid
+              fundamentals in data structure, object-oriented programming, HTTP
+              protocols, and personally having Java, JavaScript being my strong
+              programming language to work with.
             </p>
-            <p>Looking to move out for job location.</p>
-          </div>
+            <p className="text-justify">
+              Currently I am having training in FDM Group that further
+              specialises my skills in Java and Spring Framework.
+            </p>
+          </article>
         </div>
-        <div id="skills" className="mr-3 ml-3 skill-icon">
+        <div id="skillsets" className="mr-3 ml-3 flex-1">
           <SkillIcon icon="fab fa-java" caption="Java" />
           <SkillIcon icon="fab fa-python" caption="Python" />
-          <SkillIcon icon="fab fa-html5" caption="HTML" />
-          <SkillIcon icon="fab fa-css3-alt" caption="CSS" />
+          <SkillIcon icon="fab fa-sass" caption="Sass" />
           <SkillIcon icon="fab fa-js-square" caption="JavaScript" />
           <SkillIcon icon="fab fa-react" caption="ReactJS" />
           <SkillIcon icon="fab fa-node" caption="Node.JS" />
+          <SkillIcon icon="fab fa-git-alt" caption="git" />
           <SkillIcon icon="fab fa-gulp" caption="gulp" />
         </div>
       </section>
