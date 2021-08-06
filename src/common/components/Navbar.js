f47233d9b1951910d49a1f515560fb8f207c280b
smileycat <liaocrong@outlook.com>
2021-08-07 00:54:42 +1000

    refactor: navbar into functional component

--- src/common/components/Navbar.js
+++ src/common/components/Navbar.js
@@ -1,68 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-class Navbar extends React.Component {
-  state = {
-    blackNav: null,
-    showMenu: false,
+function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [navClass, setNavClass] = useState('');
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleBackdrop);
+    return () => window.removeEventListener('scroll', handleBackdrop);
+  }, []);
+
+  const handleBackdrop = () => {
+    let height = window.innerHeight - 35;
+    document.documentElement.scrollTop > height
+      ? setNavClass('opaque-black animated slideInDown fast')
+      : setNavClass('');
   };
 
-  componentDidMount() {
-    window.addEventListener('scroll', this.navBackdrop);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.navBackdrop);
-  }
-
-  toggleMenu = () => {
-    this.setState({ showMenu: !this.state.showMenu });
-  }
-
-  navBackdrop = () => {
-    let height = window.innerHeight - 35;
-    if (document.documentElement.scrollTop > height)
-      this.setState({ blackNav: 'opaque-black animated slideInDown fast' });
-    else
-      this.setState({ blackNav: null });
-  }
-
-  render() {
-    let navRight = 'nav-right';
-    navRight += this.state.showMenu ? ' nav-vertical' : '';
-    return (
-      <nav className={this.state.blackNav}>
-        <div className="nav-left">
-          <h4 id="logo">chihjung</h4>
+  return (
+    <nav className={navClass}>
+      <div className="nav-left">
+        <h4 id="logo">chihjung</h4>
+      </div>
+      <div id="nav-list" className={`nav-right ${showMenu ? 'nav-vertical' : ''}`}>
+        <div className="nav-hide" onClick={() => setShowMenu(!showMenu)}>
+          <i className="fas fa-bars"></i>
         </div>
-        <div id="nav-list" className={navRight}>
-          <div className="nav-hide" onClick={this.toggleMenu}>
-            <i className="fas fa-bars"></i>
-          </div>
-          <div className="nav-link">
-            <a href="#about">About</a>
-          </div>
-          <div className="nav-link">
-            <a href="#portfolio">Portfolio</a>
-          </div>
-          <div className="nav-link">
-            <a href="mailto:liaocrong@outlook.com">
-              <i className="fas fa-envelope"></i>
-            </a>
-          </div>
-          <div className="nav-link">
-            <a href="https://github.com/smileycat" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-github"></i>
-            </a>
-          </div>
-          <div className="nav-link">
-            <a href="https://www.linkedin.com/in/chih-jung-liao/" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-linkedin"></i>
-            </a>
-          </div>
+        <div className="nav-link">
+          <a href="#about">About</a>
         </div>
-      </nav>
-    );
-  }
+        <div className="nav-link">
+          <a href="#portfolio">Portfolio</a>
+        </div>
+        <div className="nav-link">
+          <a href="mailto:liaocrong@outlook.com">
+            <i className="fas fa-envelope"></i>
+          </a>
+        </div>
+        <div className="nav-link">
+          <a href="https://github.com/smileycat" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-github"></i>
+          </a>
+        </div>
+        <div className="nav-link">
+          <a
+            href="https://www.linkedin.com/in/chih-jung-liao/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-linkedin"></i>
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
